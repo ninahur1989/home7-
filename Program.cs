996aca82455ec7e6d9f6fff7b617dff4f7e1825c
ninahur1989 <79771234+ninahur1989@users.noreply.github.com>
2022-02-22 14:09:04 +0200

    start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace home7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
